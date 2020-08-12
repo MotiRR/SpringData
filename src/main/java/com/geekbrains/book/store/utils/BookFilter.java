45835e3a227685cc3c13cd7b1478d66319b762d6
MotiRR <motigullin.ra@gmail.com>
2020-08-12 12:20:26 +0300

    init SpringData

--- src/main/java/com/geekbrains/book/store/utils/BookFilter.java
+++ src/main/java/com/geekbrains/book/store/utils/BookFilter.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @Getter
 public class BookFilter {
-    private Specification<Book> spec;
+    private Specification<Book> spec, specGenre;
     private String filterParams;
     private Map<String, String> params;
 
@@ -25,6 +25,7 @@ public class BookFilter {
         if(genres != null)
             addNewParameters(params, genres);
         spec = Specification.where(null);
+        specGenre = Specification.where(null);
         if (params.containsKey("maxPrice") && !params.get("maxPrice").isEmpty()) {
             spec = spec.and(BookSpecifications.priceLesserOrEqualsThan(Integer.parseInt(params.get("maxPrice"))));
         }
@@ -34,10 +35,12 @@ public class BookFilter {
         if (params.containsKey("titlePart") && !params.get("titlePart").isEmpty()) {
             spec = spec.and(BookSpecifications.titleLike(params.get("titlePart")));
         }
+
         for(Genre genre : Genre.values()) {
             if (params.containsKey(genre.toString()) && !params.get(genre.toString()).isEmpty()) {
-                spec = spec.or(BookSpecifications.genreEquals(genre.getGenre()));
+                specGenre = specGenre.or(BookSpecifications.genreEquals(genre.getGenre()));
             }
         }
+        spec = spec.and(specGenre);
     }
 }
